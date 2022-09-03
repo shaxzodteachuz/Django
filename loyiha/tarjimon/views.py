5ba94fb4dab043fb6b8a9487ba4771ed52d2dd8e
shaxzodteachuz <shaxzodzoirov60@gmail.com>
2022-09-03 09:40:05 +0500

    first commit

---
+++ loyiha/tarjimon/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lugat
+
+
+
+def index(request):
+   soz = request.GET.get('q', '')
+   if soz and soz != '':
+      natija = Lugat.objects.filter(inglizcha__contains=soz).all()[:3]
+   else:
+      natija = None
+
+
+   return render(request, 'index.html', {'q':soz, 'natija': natija})
+
+# Create your views here.
+def salom2(request):
+    return HttpResponse('MENING SAHIFAM !!!')
